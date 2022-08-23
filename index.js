4b56789005dd29af1d0904ad958c4bdd0cffdc90
Sheila <rugut.cheruto@student.moringaschool.com>
2022-08-23 20:02:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', init);
+  function init() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+  }
+  console.log('DOM fully loaded and parsed');
